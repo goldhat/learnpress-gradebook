2bafe0bfd73b2e768a59b7d9064955166eac2720
Joel Milne <joel@goldhat.ca>
2020-02-17 21:21:45 -0600

    fix cron hooks, sketched run report on cron

--- watupro-gradebook/watupro-gradebook.php
+++ watupro-gradebook/watupro-gradebook.php
@@ -28,15 +28,56 @@ class WatuProGradeBook {
 		add_action( 'template_redirect',array('WatuProGradeBook', 'downloadReport'));
 		add_action( 'rwmb_enqueue_scripts', array('WatuProGradeBook', 'script'));
 
+		add_action( 'watu_gradebook_reporter', array('WatuProGradeBook', 'cronRun'));
+		add_filter( 'cron_schedules', array('WatuProGradeBook', 'cronSchedule'));
+
+	}
+
+	public static function cronRun() {
+
+		// test cron running
+		update_option( 'watupro_gradebook_log', time() );
+
+		// get gradebooks to run reports
+		$args = array(
+			'numberposts' => -1,
+			'post_type'   => 'gradebook_class'
+		);
+		$gradebooks = get_posts( $args );
+
+		// get one gradebook to report
+		$gradebook = $gradebook[0];
+
+		$gbc = new WatuProGradeBookClasses;
+		$gbc->createReport( $gradebook->ID );
+
+		/*
+		 * Break createReport() into smaller functions
+		 * We should be able to fetch just 1 user at a time
+		 * For now we can fetch all stats for 1 user
+		 * Whether we continue to fetch 2+ users is determined by how many exams
+		 */
+
+	}
+
+	public static function cronSchedule( $schedules ) {
+		if( !isset( $schedules['5min'] )) {
+			$schedules['5min'] = array(
+				'interval' => 5*60,
+				'display' => __('Every 5 minutes')
+			);
+    }
+    return $schedules;
 	}
 
 	public static function initCronSchedule() {
+	  wp_schedule_event( time(), '5min', 'watu_gradebook_reporter' );
+	}
 
-		$timestamp = wp_next_scheduled( 'watu_gradebook_reporter' );
+	public static function removeCronSchedule() {
 
-		if( $timestamp == false ) {
-		  wp_schedule_event( time(), 'daily', 'watu_gradebook_reporter' );
-		}
+		 $timestamp = wp_next_scheduled( 'watu_gradebook_reporter' );
+	   wp_unschedule_event( $timestamp, 'watu_gradebook_reporter' );
 
 	}
 
@@ -70,4 +111,5 @@ new WatuProGradeBook;
 /*
  * Cron scheduling
  */
-register_activation_hook( '__FILE__', array('WatuProGradeBook', 'initCronSchedule'));
+register_activation_hook( WATUPRO_GRADEBOOK_PATH . '/watupro-gradebook.php', array('WatuProGradeBook', 'initCronSchedule'));
+register_deactivation_hook( WATUPRO_GRADEBOOK_PATH . '/watupro-gradebook.php', array('WatuProGradeBook', 'removeCronSchedule'));
