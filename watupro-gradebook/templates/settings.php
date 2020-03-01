aca3ae9e971f2b14f22e6a342dc81f6df79e077a
Casey Milne <joel@goldhat.ca>
2020-02-29 22:54:04 -0600

    style fix template

--- watupro-gradebook/templates/settings.php
+++ watupro-gradebook/templates/settings.php
@@ -1,19 +1,23 @@
-<h1>WatuPro Gradebook Settings</h1>
+<div class="wrap">
 
-<form
-  action="<?php echo esc_url( admin_url( 'admin-post.php' )); ?>"
-  id="admin_form"
-  method="POST">
+  <h1>WatuPro Gradebook Settings</h1>
 
-  <input type="hidden" name="action" value="watupro_gradebook_save_settings">
+  <form
+    action="<?php echo esc_url( admin_url( 'admin-post.php' )); ?>"
+    id="admin_form"
+    method="POST">
 
-  <p>
-    <label>Reporting rows per cron run</label><br />
-    <input id="watupro_gradebook_rows_per_run" name="watupro_gradebook_rows_per_run" type="text" value="<?php print $rows_per_run; ?>" />
-  </p>
+    <input type="hidden" name="action" value="watupro_gradebook_save_settings">
 
-  <p>
-    <input type="submit" value="Save Settings" />
-  </p>
+    <p>
+      <label>Reporting rows per cron run</label><br />
+      <input id="watupro_gradebook_rows_per_run" name="watupro_gradebook_rows_per_run" type="text" value="<?php print $rows_per_run; ?>" />
+    </p>
 
-</form>
+    <p>
+      <input type="submit" value="Save Settings" />
+    </p>
+
+  </form>
+
+</div>
